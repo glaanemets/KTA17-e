4463720904ff46809026f7ad6caa21aa34a9d22f
glaanemets <32923795+glaanemets@users.noreply.github.com>
2017-10-29 22:53:27 +0200

    Initial commit

---
+++ kodu1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace kodu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int kArv = 100, rArv = random.Next(0, 100);
+            Console.WriteLine("See on arvamismäng\nPaku mingit arvu vahemikus 1-99");
+            while (true)
+            {
+                Console.Write("Paku arvu: ");
+                kArv = int.Parse(Console.ReadLine());
+                if (kArv < rArv)
+                {
+                    Console.WriteLine("Arv liiga väike");
+                }
+                else if (kArv > rArv)
+                {
+                    Console.WriteLine("Arv liiga suur");
+                }
+                else if (kArv == rArv)
+                {
+                    Console.WriteLine($"Pakkusid õigesti {kArv}");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
